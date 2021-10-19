e0a783555550dbb44597c8f9cec1a3bf84f7850f
Balogh P <peter.balogh1987@gmail.com>
2021-10-19 13:57:07 +0200

    My first java task

---
+++ week-02/day-02/firstJavaTask/src/HelloOthers.java
@@ -0,0 +1,9 @@
+public class HelloOthers {
+
+    public static void main(String[] args) {
+        System.out.println("Helló Donát!");
+        System.out.println("Helló Attila!");
+        System.out.println("Helló Gábor!");
+
+    }
+}
