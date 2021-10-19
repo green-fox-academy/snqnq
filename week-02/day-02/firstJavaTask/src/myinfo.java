6424fa07bb2a272978c509acd00edacb6480c186
Balogh P <peter.balogh1987@gmail.com>
2021-10-19 14:17:49 +0200

    introuce yourself added

---
+++ week-02/day-02/firstJavaTask/src/myinfo.java
@@ -0,0 +1,15 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+
+        string name = "Balogh Péter";
+        int age = 34;
+        double height = 1.79;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+
+
+
+    }
+}
